b5a93a7ee89d89a088d78994d260a0f18be63635
lantice3720 <65650884+lantice3720@users.noreply.github.com>
2022-03-14 00:44:16 +0900

    Improved Dropdown, added Icons

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ let foldables = doc.getElementsByClassName("foldable")
 
 let foldContents
 let foldLabel
-Array.from(foldables).forEach(function(foldable) {
-    console.log("asd")
-	foldable.addEventListener('click', function() {
-        foldContents = foldable.nextElementSibling.nextElementSibling
-        foldLabel = foldable.nextElementSibling
-        console.log(foldLabel.innerHTML + ", " + foldContents.clientHeight)
-        if(foldContents.clientHeight == '0'){
-            console.log("opening dropdown: " + foldLabel.innerHTML)
-        }
-	})
-})
+// Array.from(foldables).forEach(function(foldable) {
+//     console.log("asd")
+// 	foldable.addEventListener('click', function() {
+//         foldContents = foldable.nextElementSibling.nextElementSibling
+//         foldLabel = foldable.nextElementSibling
+//
+//         if(foldLabel.getAttribute("state") == '▶'){
+//             console.log("opening dropdown: " + foldLabel.innerHTML)
+//             foldLabel.firstElementChild.innerHTML = "▼"
+//         } else {
+//             foldLabel.setAttribute("state", "▶")
+//         }
+// 	})
+// })
